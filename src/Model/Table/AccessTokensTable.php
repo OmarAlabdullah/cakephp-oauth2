400587f3e341bcf06b27d66c581dcb9bb5b2c3ee
Omar Alabdullah <omar@xel.nl>
2022-11-03 16:16:57 +0100

    php-cake oauth2 project has been created

---
+++ src/Model/Table/AccessTokensTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+/**
+ * AccessToken Model
+ *
+ * @property ClientsTable $clientsTable
+ * @property UsersTable $UsersTable
+ */
+class AccessTokensTable extends Table
+{
+    /**
+     * @param array $config Config
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        $this->settable('access_tokens');
+        $this->setPrimaryKey('id');
+        $this->setDisplayField('user_id');
+        $this->setDisplayField('client_id');
+        $this->setDisplayField('name');
+        $this->setDisplayField('scopes');
+        $this->setDisplayField('revoked');
+        $this->setDisplayField('created_at');
+        $this->setDisplayField('updated_at');
+        $this->setDisplayField('expires_at');
+//        $this->hasMany('ClientTable', [
+//            'className' => 'OAuthServer.AccessTokenScopes',
+//            'foreignKey' => 'oauth_token',
+//            'dependant' => true
+//        ]);
+        parent::initialize($config);
+    }
+}
