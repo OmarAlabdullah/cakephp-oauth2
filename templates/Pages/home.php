90d322f8eb554f04ec3f96f8c8afbe801e4f542f
Omar Alabdullah <omar@xel.nl>
2022-12-06 10:38:21 +0100

    #4155 remove unnecessary login and register button from the home page

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -14,11 +14,6 @@ $this->assign('title', 'Xel-SSO');
 ?>
 <div class="container">
     <h1>Welcome to Xel SSO</h1>
-
-    <h2>Login</h2>
-    <button><a href="/login" title="login" target="_blank">Login</a></button>
-
-    <h2>Register</h2>
-    <button><a href="/register" title="login" target="_blank">Register</a></button>
+    <img src="xel-logo.png" alt="xel">
 
 </div>
